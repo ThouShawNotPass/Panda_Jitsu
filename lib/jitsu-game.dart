81d0bffabdc03e1ec2bfdacedb836bad7e5ec1a4
Justin Shaw <realjustinshaw@gmail.com>
2020-03-23 13:42:26 -0700

    purge 4

--- lib/jitsu-game.dart
+++ lib/jitsu-game.dart
@@ -3,21 +3,14 @@ import 'dart:math'; // for random integer generation
 import 'package:flame/flame.dart'; // access Flame Util's initialDimensions
 import 'package:flame/game.dart'; // access Flame Game-Loop scaffolding
 import 'package:flutter/gestures.dart';
-import 'package:panda_jitsu/components/fly.dart'; // import the Fly class/object
-import 'package:panda_jitsu/components/house-fly.dart';
-import 'package:panda_jitsu/components/agile-fly.dart';
-import 'package:panda_jitsu/components/drooler-fly.dart';
-import 'package:panda_jitsu/components/hungry-fly.dart';
-import 'package:panda_jitsu/components/macho-fly.dart';
-import 'package:panda_jitsu/components/backyard.dart'; // import the background
+import 'package:panda_jitsu/components/dojo.dart'; // import the background
 
 // This is a JitsuGame class that has gameloop logic in it to control a jitsu game. 
 // It overrides the basic (empty) Game methods 'render' && 'update'.
 class JitsuGame extends Game {
 	Size screenSize; // the size of the current device's screen
 	double tileSize; // the tile size for current screen resolution (1/9th screen width)
-	List<Fly> flies; // keep track of the flies in the game
-	Backyard background; // keep track of the background
+	Dojo background; // keep track of the background
 	Random rand; // generates new random integers
 
 	JitsuGame() {
@@ -27,59 +20,22 @@ class JitsuGame extends Game {
 	// Paints the new canvas
 	void render(Canvas canvas) {
 		background.render(canvas);
-		flies.forEach((Fly fly) => fly.render(canvas)); // draws each fly
 	}
 
-	void update(double t) {
-		flies.forEach((Fly fly) => fly.update(t));
-		flies.removeWhere((Fly fly) => fly.isOffScreen);
-	}
+	void update(double t) {}
 
 	// Calculates the size of the current screen and updates instance variable
 	void resize(Size size) {
 		screenSize = size;
-		tileSize = size.width / 9;
+		tileSize = size.height / 9; // the screen will be 9 'tiles' high
 	}
 
-	void onTapDown(TapDownDetails d) {
-		flies.forEach((Fly fly) {
-			// check if the fly area contains the tap point
-			if (fly.flyRect.contains(d.globalPosition)) {
-				fly.kill();
-			}
-		});
-	}
+	void onTapDown(TapDownDetails d) {}
 
 	// Initialize the game. Should be called exactly once, using async to wait for screenSize
 	void initialize() async {
-		flies = List<Fly>(); // initialize a list of Fly objects
-		rand = Random();
 		resize(await Flame.util.initialDimensions()); // get dimensions (returns Future<Size>)
 		
-		background = Backyard(this);
-		spawnFly();
-	}
-
-	// spawns a fly at (50, 50)
-	void spawnFly() {
-		double x = rand.nextDouble() * (screenSize.width - (tileSize * 2.025));
-		double y = rand.nextDouble() * (screenSize.height - (tileSize * 2.025));
-		switch (rand.nextInt(5)) {
-			case 0:
-				flies.add(HouseFly(this, x, y));
-				break;
-			case 1:
-				flies.add(DroolerFly(this, x, y));
-				break;
-			case 2:
-				flies.add(AgileFly(this, x, y));
-				break;
-			case 3:
-				flies.add(MachoFly(this, x, y));
-				break;
-			case 4:
-				flies.add(HungryFly(this, x, y));
-				break;	
-		}
+		background = Dojo(this);
 	}
 }
